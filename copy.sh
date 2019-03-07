951c8690cc19994ed206bcea54caddbfdea76de0
Sebastian Haderecker <sebastian.haderecker@gmail.com>
2019-03-07 21:28:36 +0000

    Copy: Some smaller tweaks
    - Make executable
    - Rewrite comments & example command

--- copy.sh
+++ copy.sh
@@ -6,9 +6,8 @@ cd /home/ubuntu/android/lineageos/out/target/product/i9300/
 
 for i in *.zip; do
 	if [[ $i =~ 14.1 ]]; then
-		# Specify your remote server address (& user) and the destination directory here
+		# Specify your remote server & user and the destination directory
 		# Make sure you have access to the remote server via SSH key or password (-p 'password')
-		# Add your SSH key for the remote server to the local ssh-agent
-		sshpass scp -o StrictHostKeyChecking=no -r /home/ubuntu/android/lineageos/out/target/product/i9300/$i ubuntu@REMOTESERVER:/home/ubuntu/html/i9300
+		sshpass scp -o StrictHostKeyChecking=no -r /home/ubuntu/android/lineageos/out/target/product/i9300/$i USER@REMOTESERVER:/var/www/html/i9300
 	fi
 done
