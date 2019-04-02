8652067ebdfe068b087ecb27c5cd31f7ab53e761
Sebastian Haderecker <sebastian.haderecker@gmail.com>
2019-04-02 21:40:59 +0000

    Add more variables
    - Make vendor variables more specific
    - Move custom local manifest to settings variable

--- settings.sh
+++ settings.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-git_name="Test"
-git_email="test@test.com"
+git_name="Sebastian Haderecker"
+git_email="sebastian.haderecker@gmail.com"
 
 device="i9300"
 
-vendor="https://github.com/TheHADILP/proprietary_vendor_samsung.git"
-path="samsung"
+vendor_files_git="https://github.com/TheHADILP/proprietary_vendor_samsung.git"
+vendor_path="samsung"
 
 ccache="100G"
 
 ram="16G"
+
+
+custom_local_manifest="truee"
+custom_local_manifest_url="https://github.com/TheHADILP/local_manifests.git "
